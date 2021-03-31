a20ff47b3df0167dbfa6000505bee0347d4a1225
Nicola Di Candia <macbook@Macbooks-MacBook-Pro.local>
2021-03-30 19:54:11 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// const element = document.createElement("h1");
+// element.innerText = "Hello, Platzi Badges. Aqui Nicola";
+
+// const container = document.getElementById("app");
+
+// container.appendChild(element);
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello Platzi Badges from React</h1>;
+
+const container = document.getElementById("app");
+
+// ReactDOM.render(__que__, __donde__);
+
+ReactDOM.render(element, container);
