1b0dd258f2706903b3ebbdac42a1c01a2f17a3c6
Nicola Di Candia <nicoladicandiajaimes@gmail.com>
2022-08-19 00:01:41 -0500

    implement life cycle and applying prettier

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,39 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import './styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
-import Navbar from '../components/Navbar';
-import BadgeForm from '../components/BadgeForm';
-import Badge from '../components/Badge';
+import "./styles/BadgeNew.css";
+import header from "../images/badge-header.svg";
+import BadgeForm from "../components/BadgeForm";
+import Badge from "../components/Badge";
 
 class BadgeNew extends React.Component {
-	state = {
-		form: {
-			firstName: '',
-			lastName: '',
-			email: '',
-			jobTitle: '',
-			twitter: '',
-		},
-	};
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: "",
+    },
+  };
 
-	handleChange = e => {
-		this.setState({
-			form: {
-				...this.state.form,
-				[e.target.name]: e.target.value,
-			},
-		});
-	};
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-	handleSubmit = e => {
-		console.log('Submit button was clicked');
-		e.preventDefault();
-		console.log(this.state);
-	};
+  handleSubmit = (e) => {
+    console.log("Submit button was clicked");
+    e.preventDefault();
+    console.log(this.state);
+  };
 
-	render() {
-		return (
+  render() {
+    return (
       <React.Fragment>
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo" />
@@ -43,7 +42,7 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-sm-12 col-md-6 mt-4">
               <Badge
-                avatarUrl="https://media-exp1.licdn.com/dms/image/C5603AQGaZYXT3lye2A/profile-displayphoto-shrink_100_100/0/1614566026990?e=1657756800&v=beta&t=Pr_SFQ6MKKo0sdHP5bjhjbwY8ds-2jMTDjYbPArtZ2Q"
+                avatarUrl="https://media-exp1.licdn.com/dms/image/C5603AQGaZYXT3lye2A/profile-displayphoto-shrink_400_400/0/1614566026990?e=1666224000&v=beta&t=9F6COVKCPGemvamjHLZBpSWa9qJG9je4f698KVTZEOY"
                 firstName={this.state.form.firstName}
                 lastName={this.state.form.lastName}
                 jobTitle={this.state.form.jobTitle}
@@ -62,7 +61,7 @@ class BadgeNew extends React.Component {
         </div>
       </React.Fragment>
     );
-	}
+  }
 }
 
 export default BadgeNew;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-function Home () {
-  return(
-    <div style={{ display:"flex", height: "90vh" }}>
-      <h1 style={{ margin: "auto" }}>Mi Casita</h1>
+import "./styles/Home.css";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1 className="home__element">Welcome to Platzi</h1>
+      <div className="home__element">
+        <Link to="/badges" className="btn btn-primary">
+          New Badge
+        </Link>
+        {/* <button className="btn btn-primary">Go to Badges</button> */}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
